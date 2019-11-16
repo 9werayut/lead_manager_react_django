91814f102f1fd24b3f9600065fe46d8711454293
Weerayut Khumsuwan <werayut.ku@gmail.com>
2019-11-16 22:37:21 +0700

    implementation react

---
+++ leadmanager/frontend/src/components/leads/Lead.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Lead extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Lead lists</h1>
+      </div>
+    );
+  }
+}
